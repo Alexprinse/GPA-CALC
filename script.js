faee5bded6f4ebeb7a21ef81799f98179dcba425
Alexprinse <141005543+Alexprinse@users.noreply.github.com>
2023-08-07 13:49:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function main()
+{
+    window.location.href="index.html";
+}
+function result()
+{
+    if(document.getElementById("year").value == 1 && document.getElementById("branch").value == 1)
+    {
+        window.location.href="cse.html";
+    }
+    if(document.getElementById("year").value == 1 && document.getElementById("branch").value == 2)
+    {
+        window.location.href="ece.html";
+    }
+    if(document.getElementById("year").value == 1 && document.getElementById("branch").value == 3)
+    {
+        window.location.href="cse.html";
+    }
+    if(document.getElementById("year").value == 1 && document.getElementById("branch").value == 4)
+    {
+        window.location.href="cse.html";
+    }
+    if(document.getElementById("year").value == 1 && document.getElementById("branch").value == 5)
+    {
+        window.location.href="cse.html";
+    }
+}
+

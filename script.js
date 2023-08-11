e82e6d4a5a3d68aaab201876a6777a16b3bad2e4
Alexprinse <princebadampudi@gmail.com>
2023-08-11 14:12:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,5 +24,45 @@ function result()
     {
         window.location.href="me.html";
     }
+    if(document.getElementById("year").value == 2 && document.getElementById("branch").value == 1)
+    {
+        window.location.href="underc.html";
+    }
+    if(document.getElementById("year").value == 2 && document.getElementById("branch").value == 2)
+    {
+        window.location.href="underc.html";
+    }
+    if(document.getElementById("year").value == 2 && document.getElementById("branch").value == 3)
+    {
+        window.location.href="underc.html";
+    }
+    if(document.getElementById("year").value == 2 && document.getElementById("branch").value == 4)
+    {
+        window.location.href="underc.html";
+    }
+    if(document.getElementById("year").value == 2 && document.getElementById("branch").value == 5)
+    {
+        window.location.href="underc.html";
+    }
+    if(document.getElementById("year").value == 3 && document.getElementById("branch").value == 1)
+    {
+        window.location.href="cse.html";
+    }
+    if(document.getElementById("year").value == 3 && document.getElementById("branch").value == 2)
+    {
+        window.location.href="underc.html";
+    }
+    if(document.getElementById("year").value == 3 && document.getElementById("branch").value == 3)
+    {
+        window.location.href="underc.html";
+    }
+    if(document.getElementById("year").value == 3 && document.getElementById("branch").value == 4)
+    {
+        window.location.href="underc.html";
+    }
+    if(document.getElementById("year").value == 3 && document.getElementById("branch").value == 5)
+    {
+        window.location.href="underc.html";
+    }
 }
 
